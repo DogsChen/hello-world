dee7e2c87bf75c22513e1454be9cec596a119550
xswang <wxs_code@126.com>
2024-04-19 10:28:58 +0800

    jenkins-java项目

---
+++ src/main/java/com/wxs/code/helloworld/HelloWorldApplication.java
@@ -0,0 +1,31 @@
+package com.wxs.code.helloworld;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+
+    @GetMapping("t1")
+    public String aa() {
+        return "t1" + System.currentTimeMillis();
+    }
+
+    @GetMapping("t2")
+    public String t2(HttpServletRequest request) {
+        return "t2" + request.getHeaderNames() + "-" + System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(HelloWorldApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        System.out.println("项目启动成功！envs:\n" + environment.getSystemProperties());
+    }
+}
