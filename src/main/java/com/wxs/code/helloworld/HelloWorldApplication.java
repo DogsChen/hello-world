1d69265d9f9d55e458bc0fba39a9cc9ef12c0056
xswang <wxs_code@126.com>
2024-04-20 16:11:17 +0800

    项目更新-t6

--- src/main/java/com/wxs/code/helloworld/HelloWorldApplication.java
+++ src/main/java/com/wxs/code/helloworld/HelloWorldApplication.java
@@ -17,29 +17,29 @@ public class HelloWorldApplication {
     public String aa() {
         return "update-t1" + System.currentTimeMillis();
     }
-
-    @GetMapping("t2")
-    public String t2(HttpServletRequest request) {
-        return "t2" + request.getHeaderNames() + "-" + System.currentTimeMillis();
-    }
-    @GetMapping("t3")
-    public String t3(HttpServletRequest request) {
-        return "t3" + request.getHeaderNames() + "-" + System.currentTimeMillis();
-    }
-
-    @GetMapping("t4")
-    public String t4(HttpServletRequest request) {
-        return "t4" + request.getRequestURL() + "-" + System.currentTimeMillis();
-    }
-
-    @GetMapping("t5")
-    public String t5(HttpServletRequest request) {
-        return "t5" + request.getRequestURL() + "-" + System.currentTimeMillis();
-    }
-    @GetMapping("t6")
-    public String t6(HttpServletRequest request) {
-        return "t6-" + request.getRequestURL() + "-" + System.currentTimeMillis();
-    }
+//
+//    @GetMapping("t2")
+//    public String t2(HttpServletRequest request) {
+//        return "t2" + request.getHeaderNames() + "-" + System.currentTimeMillis();
+//    }
+//    @GetMapping("t3")
+//    public String t3(HttpServletRequest request) {
+//        return "t3" + request.getHeaderNames() + "-" + System.currentTimeMillis();
+//    }
+//
+//    @GetMapping("t4")
+//    public String t4(HttpServletRequest request) {
+//        return "t4" + request.getRequestURL() + "-" + System.currentTimeMillis();
+//    }
+//
+//    @GetMapping("t5")
+//    public String t5(HttpServletRequest request) {
+//        return "t5" + request.getRequestURL() + "-" + System.currentTimeMillis();
+//    }
+//    @GetMapping("t6")
+//    public String t6(HttpServletRequest request) {
+//        return "t6-" + request.getRequestURL() + "-" + System.currentTimeMillis();
+//    }
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run = SpringApplication.run(HelloWorldApplication.class, args);
